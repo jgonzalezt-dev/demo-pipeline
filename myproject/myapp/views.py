d62bc912d2686174c27581939f241c3a24951c50
jgonzalez <jgonzalez.gyf@gmail.com>
2025-04-14 20:48:23 -0400

    Agrega fecha actual a la vista de ping

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from datetime import datetime
+
 
 # Create your views here.
 from django.http import JsonResponse
 
 def ping(request):
-    return JsonResponse({'ping': 'pong'})
+    return JsonResponse({'ping': 'pong', 
+                         'date': datetime.now().isoformat()})
+
 
